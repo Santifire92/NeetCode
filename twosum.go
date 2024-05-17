83290c33fb9c1737538570eb45a235344143c514
Santifire92 <camiloujeda@gmail.com>
2024-05-16 21:19:17 -0300

    implement solution with better complexity

---
+++ twosum.go
@@ -0,0 +1,34 @@
+package twosum
+
+import "sort"
+
+// O(n^2) solution, not pretty but does the job!
+
+func twoSumOn2(nums []int, target int) []int {
+	sort.Ints(nums)
+	var wanted int
+	for i := range nums {
+		wanted = target - nums[i]
+		for j, num := range nums[i+1:] {
+			if num == wanted {
+				return []int{i, i + j + 1}
+			}
+		}
+	}
+	return nil
+}
+
+func twoSumOn(nums []int, target int) []int {
+	// Create a map to store the values and their index
+	m := make(map[int]int)
+	for i, num := range nums {
+		// Check if the difference between the target and the current number is in the map
+		if j, ok := m[target-num]; ok {
+			return []int{j, i}
+		}
+		// Store the number and its index in the map
+		m[num] = i
+	}
+	return nil
+}
+
