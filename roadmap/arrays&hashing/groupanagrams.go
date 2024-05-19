a60f8dd6652a11a90e636f544dc25b02bd6b25b4
Santifire92 <camiloujeda@gmail.com>
2024-05-19 16:21:17 -0300

    Implement simple yet efective solution

--- roadmap/arrays&hashing/groupanagrams.go
+++ roadmap/arrays&hashing/groupanagrams.go
@@ -1,4 +1,26 @@
 package arraysandhashing
+
+import "sort"
+
 func groupAnagrams(strs []string) [][]string {
-    for 
+	hashingTable := make(map[string][]string)
+	for _, str := range strs {
+		sortedStr := sortString(str)
+		if anagramsBag, ok := hashingTable[sortedStr]; ok {
+			hashingTable[sortedStr] = append(anagramsBag, str)
+		} else {
+			hashingTable[sortedStr] = []string{str}
+		}
+	}
+	result := make([][]string, 0, len(hashingTable))
+	for _, anagramsBag := range hashingTable {
+		result = append(result, anagramsBag)
+	}
+	return result
+}
+
+func sortString(s string) string {
+	r := []rune(s)
+	sort.Slice(r, func(i, j int) bool { return r[i] < r[j] })
+	return string(r)
 }
